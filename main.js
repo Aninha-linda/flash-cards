2448ec88181cce9ecbb04bafa428597a98990c75
Ana Carolina <129754161+Aninha-linda@users.noreply.github.com>
2024-09-19 08:50:33 -0300

    .

---
+++ main.js
@@ -0,0 +1,20 @@
+function CriaCartao (categoria, pergunta, resposta){
+    let conteiner = document.getElementById('conteiner');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = `
+    <div class="conteudo-cartao">
+    <h3>${categoria}</h3>
+
+<div class="pergunta-cartao">
+    <p>${pergunta}</p>
+
+</div>
+<div class="resposta-cartao">
+    <p>${resposta}</p>
+
+</div>
+</div>`
+
+
+}
