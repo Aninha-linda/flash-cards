d30672fe091837e2663991ff423dcac86bf47184
Ana Carolina <129754161+Aninha-linda@users.noreply.github.com>
2024-09-19 07:38:25 -0300

    u

---
+++ perguntas.js
@@ -0,0 +1,99 @@
+CriaCartao(
+' História ',
+' Como começou a Revolução Francesa? ',
+' Com a queda de Bastilha ')
+
+CriaCartao(
+' Geografia ',
+' Qual o clima da região sul do país? ',
+' Subtropical ')
+
+CriaCartao(
+' Sociologia ',
+' Quem escreveu o Manifesto Comunista? ',
+' Karl Marx e Marx Engel ')
+
+CriaCartao(
+' Filosofia ',
+' Qual o conceito mais famoso da autora Hannah Arendt? ',
+' A Banalização do Mal ')
+
+CriaCartao(
+' Geopolítica ',
+' O que aconteceu em 11 de setembro de 2001? ',
+' O atentado terrorista às torres gêmeas ')
+
+CriaCartao(
+' Biologia ',
+' Qual o fluido que transporta oxigênio pelo corpo? ',
+' O sangue ')
+
+CriaCartao(
+' Física ',
+' Qual a unidade de medida do S.I para unidade de tempo? ',
+' Segundos ')
+
+CriaCartao(
+' Química ',
+' Qual a química do carbono? ',
+' Química Orgânica ')
+
+CriaCartao(
+' Matemática ',
+' Qual a fórmula para se calcular os termos de uma P.A? ',
+' sn= (a1 + an) - n / 2 ')
+
+CriaCartao(
+' História ',
+' Quando o homem foi para a Lua? ',
+' em 03 de outubro de 1969 ')
+
+CriaCartao(
+' Geografia ',
+' Qual o bioma de maior extensão na região Nordeste do Brasil? ',
+' Caatinga ')
+
+CriaCartao(
+' Sociologia ',
+' Quem escreveu o livro “O Capital” ?',
+' Karl Marx ')
+
+CriaCartao(
+' Filosofia ',
+' Quem são os filósofos gregos mais importantes da antiguidade? ',
+' Aristóteles, Sócrates e Platão ')
+
+CriaCartao(
+' Geopolítica ',
+' Os Estados Unidos pertencem a qual bloco econômico? ',
+' O Bloco Capitalista ')
+
+CriaCartao(
+' Biologia ',
+' Qual o sistema do corpo que bombeia o sangue? ',
+' Sistema Cardiovascular')
+
+CriaCartao(
+' Física ',
+' Quais são os pólos magnéticos?',
+' Pólo sul e norte magnético')
+
+CriaCartao(
+' Química ',
+' Qual o ph básico? ',
+' ph 10 ')
+
+CriaCartao(
+' Matemática ',
+' Em graus, qual o valor de π? ',
+' 180° graus ')
+
+CriaCartao(
+' Literatura ',
+' Quem é o autor brasileiro modernista mais importante da literatura? ',
+' Machado de Assis ')
+
+CriaCartao(
+' Inglês ',
+' Are you going to school? ',
+' Yes ')
